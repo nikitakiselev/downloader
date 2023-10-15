eb317706627d86e45c96407168e062c009b8c4da
Nikita Kiselev <mail@nikitakiselev.ru>
2023-10-15 16:41:17 +0300

    feat(docs): laravel swagger docs configuration

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,6 +6,27 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+ * @OA\Info(
+ *     description="API to download everything from the Internet.",
+ *     version="1.0.0",
+ *     title="Downloader API",
+ *     @OA\Contact(email="mail@nikitakiselev.ru"),
+ *     @OA\License(
+ *         name="Apache 2.0",
+ *         url="http://www.apache.org/licenses/LICENSE-2.0.html"
+ *     )
+ * )
+ *
+ * @OA\SecurityScheme(
+ *      type="apiKey",
+ *      in="header",
+ *      securityScheme="api_key",
+ *      name="api_key"
+ *  )
+ *
+ * @OA\Server(url="/api")
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
